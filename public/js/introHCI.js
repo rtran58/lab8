154c82d3f1011a3a3f9ac0cdce26170276dda756
vagrant <vagrant@precise32.(none)>
2014-02-27 01:51:14 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    $(".likeBtn").click(function(e) {
+        console.log("clicked");
+        ga('create', 'UA-48469109-1', 'rtran58lab8.herokuapp.com');
+        ga('send', 'event', 'like', 'click');
+    });
 }
